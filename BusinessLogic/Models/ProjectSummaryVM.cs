107888ccd9f4870ef04a89c9384bbaeb4657789e
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-12-04 21:54:47 +0100

    Renamed directories

---
+++ BusinessLogic/Models/ProjectSummaryVM.cs
@@ -0,0 +1,10 @@
+﻿namespace BusinessLogic.Models;
+
+public class ProjectSummaryVM
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = default!;
+    public string? Description { get; set; }
+    public string ManagerName { get; set; } = default!;
+    public int NumberOfTasks { get; set; }
+}
